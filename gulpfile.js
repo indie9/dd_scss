1d2d1b630831f87b728c72be5a18b80d696857d4
nikolay galanov <galanovn@gmail.com>
2022-04-02 18:37:18 +0300

    add all files

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const sass = require('sass');
+const gulpSass = require('gulp-sass');
+
+const scssCompiler = gulpSass(sass);
+
+gulp.task('styles', () => {
+   return gulp.src('./scss/stl.scss')
+        .pipe(scssCompiler().on('error', scssCompiler.logError))
+        .pipe(gulp.dest('./css'))
+});
+
+gulp.task('watch', () => {
+    return gulp.watch('./scss/*.scss', gulp.series('styles'));
+});
+
+//npm run build:scss 
